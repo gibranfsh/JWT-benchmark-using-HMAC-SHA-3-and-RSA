2e0511a4e2f20c193a6e23db00afb23b610da72a
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-06-12 18:33:40 +0700

    feat: added tester function calls on main.go

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 // File: main.go
-// Berikut adalah program untuk benchmarking pembuatan dan verifikasi token JWT dengan algoritma RSA dan HMAC SHA-3.
 
 package main
-
-import (
-	"crypto/rsa"
-	"fmt"
-	"time"
-)
 
 func main() {
 	// Generate RSA keys
@@ -24,54 +17,20 @@ func main() {
 
 	// Benchmarking HMAC SHA-3
 	benchmarkHMACSHA3(hmacSecret)
-}
 
-// Function to benchmark RSA token creation and verification
-func benchmarkRSA(privateKey *rsa.PrivateKey, publicKey *rsa.PublicKey) {
-	// Benchmark token creation
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		createRSAToken(privateKey)
-	}
-	creationTime := time.Since(start)
+	// Test integrity check
+	integrityCheckRSA(publicKey)
+	integrityCheckHMAC()
 
-	// Benchmark token verification
-	tokenString, _ := createRSAToken(privateKey)
+	// Test expiration handling
+	expirationHandling(privateKey, publicKey)
+	
+	// Test algorithm confusion
+	algorithmConfusion(privateKey, publicKey, hmacSecret)
 
-	// print tokenString
-	fmt.Println("JWT hasil generasi menggunakan RSA: ", tokenString+"\n")
+	// Test payload size impact
+	payloadSizeImpact(privateKey, publicKey, hmacSecret)
 
-	start = time.Now()
-	for i := 0; i < 1000; i++ {
-		verifyRSAToken(tokenString, publicKey)
-	}
-	verificationTime := time.Since(start)
-
-	fmt.Println("1000x JWT Creation using RSA Time:", creationTime)
-	fmt.Println("1000x JWT Verification using RSA Time:", verificationTime)
-}
-
-// Function to benchmark HMAC SHA-3 token creation and verification
-func benchmarkHMACSHA3(hmacSecret []byte) {
-	// Benchmark token creation
-	start := time.Now()
-	for i := 0; i < 1000; i++ {
-		createHMACSHA3Token(hmacSecret)
-	}
-	creationTime := time.Since(start)
-
-	// Benchmark token verification
-	tokenString, _ := createHMACSHA3Token(hmacSecret)
-
-	// print tokenString
-	fmt.Println("\nJWT hasil generasi menggunakan HMAC SHA-3: ", tokenString+"\n")
-
-	start = time.Now()
-	for i := 0; i < 1000; i++ {
-		verifyHMACSHA3Token(tokenString, hmacSecret)
-	}
-	verificationTime := time.Since(start)
-
-	fmt.Println("1000x JWT Creation using HMAC SHA-3 Time:", creationTime)
-	fmt.Println("1000x JWT Verification using HMAC SHA-3 Time:", verificationTime)
+	// Stress testing
+	stressTesting(privateKey, publicKey, hmacSecret)
 }
