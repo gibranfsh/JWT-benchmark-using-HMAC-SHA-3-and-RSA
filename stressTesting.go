325efb929e7d9d4c991194cca3131145924763a5
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2024-06-12 18:33:15 +0700

    feat: added stress testing for RSA and HMAC SHA-3 JWT

---
+++ stressTesting.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/rsa"
+	"fmt"
+	"time"
+)
+
+// Function to test stress testing by creating and verifying tokens in parallel
+func stressTesting(privateKey *rsa.PrivateKey, publicKey *rsa.PublicKey, hmacSecret []byte) {
+	numGoroutines := 100
+	done := make(chan bool, numGoroutines)
+
+	// Stress test RSA
+	start := time.Now()
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				tokenString, _ := createRSAToken(privateKey)
+				verifyRSAToken(tokenString, publicKey)
+			}
+			done <- true
+		}()
+	}
+	for i := 0; i < numGoroutines; i++ {
+		<-done
+	}
+	fmt.Println("RSA stress test time:", time.Since(start))
+
+	// Stress test HMAC SHA-3
+	start = time.Now()
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				tokenString, _ := createHMACSHA3Token(hmacSecret)
+				verifyHMACSHA3Token(tokenString, hmacSecret)
+			}
+			done <- true
+		}()
+	}
+	for i := 0; i < numGoroutines; i++ {
+		<-done
+	}
+	fmt.Println("HMAC SHA-3 stress test time:", time.Since(start))
+}
